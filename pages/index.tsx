4dcf644028d0930dc479b9e233750cf9be48426d
Ayman Kassab <aymkas@gmail.com>
2022-06-09 06:08:43 -0400

    seperate posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
+import {
+  getSortedPostProjectData,
+  getSortedPostExperiencesData,
+} from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 import { Avatar, Grid } from '@mui/material';
 
-const Home = ({ allPostsData }) => {
+const Home = ({ allPostExperienceData, allPostProjectData }) => {
   return (
     <Layout home>
       <Head>
@@ -20,9 +23,9 @@ const Home = ({ allPostsData }) => {
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Experiences</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title, img }) => (
+          {allPostExperienceData.map(({ id, date, title, img }) => (
             <li className={utilStyles.listItem} key={id}>
               <Grid container>
                 <Grid item xs={8} className={utilStyles.listItemTextContainer}>
@@ -52,15 +55,46 @@ const Home = ({ allPostsData }) => {
           ))}
         </ul>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Projects</h2>
+        <ul className={utilStyles.list}>
+          {allPostProjectData.map(({ id, date, title, img }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Grid container>
+                <Grid item xs={8} className={utilStyles.listItemTextContainer}>
+                  <Link href={`/posts/${id}`}>
+                    <a>{title}</a>
+                  </Link>
+                </Grid>
+                <Grid>
+                  <small className={utilStyles.lightText}>
+                    <Date dateString={date} />
+                  </small>
+                </Grid>
+              </Grid>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Experiences</h2>
+        <ul className={utilStyles.list}>
+          <li className={utilStyles.listItem}>API Wrapper</li>
+          <li className={utilStyles.listItem}>AI Breaker</li>
+          <li className={utilStyles.listItem}>Social Sports</li>
+        </ul>
+      </section>
     </Layout>
   );
 };
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const allPostExperienceData = getSortedPostExperiencesData();
+  const allPostProjectData = getSortedPostProjectData();
   return {
     props: {
-      allPostsData,
+      allPostExperienceData,
+      allPostProjectData,
     },
   };
 }
